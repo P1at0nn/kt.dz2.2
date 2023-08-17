60d1cd695638bd074ea27487c04bb94a759b1df6
P1at0nn <131589712+P1at0nn@users.noreply.github.com>
2023-08-17 22:17:42 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
 val likes =501
 
-    if ((likes -1) % 10 == 0  ==true && likes !==11 )
+    if ((likes - 1) % 10 == 0  == true && likes !== 11 )
      println("Понравилось $likes человеку")
        else println("Понравилось $likes людям")
 
