12e1acd166953f4c9491f272060900918029799f
platon an47 <verzunovplat0n@gmail.com>
2023-08-17 22:15:59 +0300

    new commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+val likes =501
+
+    if ((likes -1) % 10 == 0  ==true && likes !==11 )
+     println("Понравилось $likes человеку")
+       else println("Понравилось $likes людям")
+
+}
